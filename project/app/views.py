25447c71ee592a696049b5175066f3b888d7969b
Nick <nickconnors27@gmail.com>
2023-04-26 21:49:06 -0400

    log in user after registering

--- project/app/views.py
+++ project/app/views.py
@@ -17,6 +17,12 @@ def registerUser(request):
         form = SignupForm(data=request.POST)
         if form.is_valid():
             form.save()
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+
             return redirect('index')
     else:
         form = SignupForm()
